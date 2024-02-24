f148fdea42b3d3d72d48b667ec258f8d1d6a3585
suarez__alvarez <martin.suarezalvarez1@gmail.com>
2024-02-24 23:30:54 +0100

    Integration of changes

---
+++ apis.py
@@ -0,0 +1,86 @@
+
+import requests
+from flask import Flask, request, send_file
+
+def get_sequence(uniprot_accession_code):
+    url = f'https://rest.uniprot.org/uniprotkb/{uniprot_accession_code}.fasta'
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.text.strip()
+    else:
+        return "Could not retrieve sequence information", 400
+
+
+def get_function(uniprot_accession_code):
+    url = f'https://www.uniprot.org/uniprot/{uniprot_accession_code}.txt'
+    response = requests.get(url)
+    if response.status_code == 200:
+        function_paragraph = ""
+        hola = False
+        for line in response.text.split('\n'):
+            if '-!- FUNCTION' in line:
+                hola = True
+                line = line.replace('-!- FUNCTION:', '')
+            elif '-!- SUBCELLULAR' in line:
+                hola = False
+            if hola:
+                line = line.replace('CC   ', '')
+                function_paragraph += line.strip() + " "
+    else:
+        return "Could not retrieve function information", 400
+    
+
+
+def get_network(uniprot_accession_code):
+    string_api_url = "https://version-11-5.string-db.org/api"
+    output_format1 = "tsv-no-header"
+    output_format3 = "image"
+    method1 = "get_string_ids"
+    method2 = "network"
+
+    params1 = {
+        "identifiers": uniprot_accession_code,
+        "limit": 1,
+        "echo_query": 1,
+    }
+
+    params2 = {
+        "identifiers": uniprot_accession_code,
+        "add_white_nodes": 15,
+        "network_flavor": "confidence",
+        "caller_identity": "www.awesome_app.org"
+    }
+
+    request_url_id = "/".join([string_api_url, output_format1, method1])
+    request_url_network = "/".join([string_api_url, output_format3, method2])
+
+    results_id = requests.post(request_url_id, data=params1)
+    results_network = requests.post(request_url_network, data=params2)
+
+    string_identifier = None
+    for line in results_id.text.strip().split("\n"):
+        l = line.split("\t")
+        input_identifier, string_identifier = l[0], l[2]
+
+    file_name = f"{uniprot_accession_code}_network.png"
+    with open(file_name, 'wb') as fh:
+        fh.write(results_network.content)
+
+    return send_file(file_name, mimetype='image/png')
+
+
+def fetch_nucleotide_sequence(nucleotide_id):
+    api_url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi"
+    params = {
+        "db": "nuccore",
+        "id": nucleotide_id,
+        "rettype": "fasta",
+        "retmode": "text"
+    }
+    
+    response = requests.get(api_url, params=params)
+    
+    if response.status_code == 200:
+        sequence = response.text
+    else:
+        return "Failed to fetch nucleotide sequence from NCBI. Status code: " + str(response.status_code), 400
--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, url_for, redirect, request
-from model import db
+from flask import Flask, render_template, url_for, redirect, request 
+from model import *
 from forms import SignUpForm, LoginForm, SearchForm
-
 import pymysql
+from apis import *
+from sqlalchemy import select, and_ 
 
 app = Flask(__name__)
 
@@ -29,96 +30,41 @@ def signup():
 
 @app.route('/search' , methods = ['POST','GET'])
 def search():
+
+
     gene_input = request.form.get('gene_name')
     species = request.form.get('species')
-    return render_template('search.html' , gene_input = gene_input, species = species)
 
-@app.route('/sequence/<uniprot_accession_code>')
-def get_sequence(uniprot_accession_code):
-    url = f'https://rest.uniprot.org/uniprotkb/{uniprot_accession_code}.fasta'
-    response = requests.get(url)
-    if response.status_code == 200:
-        sequence = response.text.strip()
-        return render_template('sequence.html', sequence=sequence)
+    # Query the Gene table for a gene with the given name and species
+    gene = Gene.query.filter_by(gene_name=gene_input, species=species).first()
+
+    # Get the Database instance for Uniprot
+    uniprot = Database.query.filter_by(db_name='Uniprot').first()
+
+ 
+        
+    if gene and uniprot:
+        
+        # Create a select statement for the gene_has_database table
+        stmt = select(gene_has_database.c.gene_in_db).where(and_(gene_has_database.c.gene_id == gene.id, gene_has_database.c.database_id == uniprot.id))
+
+        # Execute the select statement and fetch the results
+        results = db.session.execute(stmt).fetchall()
+
+        # Print the gene_in_db values
+        for result in results:
+            uniprot_accession_code = result[0]
+        
+
+        sequence = get_sequence(uniprot_accession_code=uniprot_accession_code)
+        
+
+        return render_template('search.html', gene_input=gene.gene_name, uniprot_accession_code=uniprot_accession_code , species = gene.species, sequence = sequence)
+
+
     else:
-        return "Could not retrieve sequence information", 400
-
-@app.route('/function/<uniprot_accession_code>')
-def get_function(uniprot_accession_code):
-    url = f'https://www.uniprot.org/uniprot/{uniprot_accession_code}.txt'
-    response = requests.get(url)
-    if response.status_code == 200:
-        function_paragraph = ""
-        hola = False
-        for line in response.text.split('\n'):
-            if '-!- FUNCTION' in line:
-                hola = True
-                line = line.replace('-!- FUNCTION:', '')
-            elif '-!- SUBCELLULAR' in line:
-                hola = False
-            if hola:
-                line = line.replace('CC   ', '')
-                function_paragraph += line.strip() + " "
-        return render_template('function.html', function=function_paragraph.strip())
-    else:
-        return "Could not retrieve function information", 400
-    
-
-@app.route('/network/<uniprot_accession_code>')
-def get_network(uniprot_accession_code):
-    string_api_url = "https://version-11-5.string-db.org/api"
-    output_format1 = "tsv-no-header"
-    output_format3 = "image"
-    method1 = "get_string_ids"
-    method2 = "network"
-
-    params1 = {
-        "identifiers": uniprot_accession_code,
-        "limit": 1,
-        "echo_query": 1,
-    }
-
-    params2 = {
-        "identifiers": uniprot_accession_code,
-        "add_white_nodes": 15,
-        "network_flavor": "confidence",
-        "caller_identity": "www.awesome_app.org"
-    }
-
-    request_url_id = "/".join([string_api_url, output_format1, method1])
-    request_url_network = "/".join([string_api_url, output_format3, method2])
-
-    results_id = requests.post(request_url_id, data=params1)
-    results_network = requests.post(request_url_network, data=params2)
-
-    string_identifier = None
-    for line in results_id.text.strip().split("\n"):
-        l = line.split("\t")
-        input_identifier, string_identifier = l[0], l[2]
-
-    file_name = f"{uniprot_accession_code}_network.png"
-    with open(file_name, 'wb') as fh:
-        fh.write(results_network.content)
-
-    return send_file(file_name, mimetype='image/png')
-
-@app.route('/nucleotide/<nucleotide_id>')
-def fetch_nucleotide_sequence(nucleotide_id):
-    api_url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi"
-    params = {
-        "db": "nuccore",
-        "id": nucleotide_id,
-        "rettype": "fasta",
-        "retmode": "text"
-    }
-    
-    response = requests.get(api_url, params=params)
-    
-    if response.status_code == 200:
-        sequence = response.text
-        return render_template('nucleotide.html', sequence=sequence)
-    else:
-        return "Failed to fetch nucleotide sequence from NCBI. Status code: " + str(response.status_code), 400
+        
+        return render_template(index.html)
 
 
 # Call the create_app function and run the app
